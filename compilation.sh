c712ddaa9e0999f38076e49302803393215aefca
ClemV <22980442+clementviolet@users.noreply.github.com>
2023-10-30 16:38:54 +0100

    Various update of the template

--- compilation.sh
+++ compilation.sh
@@ -1,57 +1,65 @@
 #!/bin/bash
-# echo "Test"
-
-# Main
-
 cd src
 
-pandoc-citeproc --bib2json bibliography.bib > references.json
-# python3 .assets/scripts/bibliography.py
-python3 references.py
+# Conversion Word
+pandoc -f markdown -t docx \
+chapters/00-abstract.md \
+chapters/01-introduction.md \
+chapters/02-methods.md \
+chapters/03-results.md \
+chapters/04-discussion.md \
+chapters/05-formal.md \
+-o ../rendered/preprint.docx \
+--verbose \
+--metadata-file=../metadata.json \
+--reference-doc=template/template.docx \
+--filter pandoc-crossref \
+--metadata crossrefYaml="template/pandoc_crossref_conf.yml" \
+--metadata link-bibliography="true" \
+--metadata link-citations="true" \
+--citeproc --biblatex \
+--bibliography=bibliography.bib \
+--csl=template/ecology-letters.cls
 
-# Convert the abstract for the pdf
+# Conversion TeX
+
 pandoc -f markdown -t latex chapters/00-abstract.md -o abstract.tex
 
-# Convert the main text for the pdf
-pandoc -f markdown -t markdown chapters/01-introduction.md chapters/02-methods.md \
-    chapters/03-results.md chapters/04-discussion.md \
-    chapters/05-formal.md -o main.md
+## Convert the main text up to the table
+pandoc -f markdown -t markdown \
+chapters/01-introduction.md \
+chapters/02-methods.md \
+chapters/03-results.md \
+chapters/04-discussion.md \
+chapters/05-formal.md \
+-o main_tex.md \
+--verbose
 
-# Convert the appendicies to for the pdf
-pandoc -f markdown-auto_identifiers -t latex supplementary/07-appendices.md -o appendices.tex --template=supplementary/99-annexe_separator.tex
-pandoc -f markdown-auto_identifiers -t latex supplementary/08-resources.md -o ressources.tex --template=supplementary/99-annexe_separator.tex
+## Create the pdf
 
-# Concatenate both files
-cat appendices.tex ressources.tex > annexes.tex
+cp template/template.tex .
+cp template/lapreprint.cls .
 
-echo "Microsof Word Document"
+latexmk -c
+pandoc -f markdown -t latex main_tex.md -o preprint.tex \
+--verbose \
+--template=template.tex \
+--metadata-file=../metadata.json \
+--filter pandoc-crossref \
+--metadata link-citations="true" \
+--metadata crossrefYaml=pandoc_crossref_conf.yml \
+--citeproc --biblatex --bibliography=bibliography.bib
 
-pandoc -f markdown -t docx chapters/00-abstract.md main.md \
-  supplementary/07-appendices.md  supplementary/08-resources.md \
-  -o preprint.docx --metadata-file=../metadata.json \
-  --filter pandoc-fignos --filter pandoc-tablenos \
-  --citeproc --bibliography=references.json \
-  --csl=template/ecology-letters.cls
+latexmk preprint.tex  -f -lualatex --file-line-error --interaction=nonstopmode && latexmk -c
 
+mv preprint.pdf ../rendered/
+mv preprint.tex ../rendered/
 
-echo "PDF Document" 
-# There is a small issue with pandoc-tablenos (multiply defined labels) but I can't figure what is happening. It seems fine on the pdf output thus.
+## Cleaning -up
+rm  main_tex.md template.tex lapreprint.cls \
+ preprint.bbl preprint.suppinfo preprint.run.xml
 
-cp template/template.tex template.tex
-cp template/lapreprint.cls lapreprint.cls
-
-pandoc -f markdown -t latex main.md -o preprint.tex --template=template.tex \
-    --filter pandoc-xnos --metadata-file=../metadata.json \
-    --citeproc --bibliography=references.json \
-    --csl=template/ecology-letters.cls
-
-latexmk preprint.tex -lualatex --file-line-error --interaction=nonstopmode && latexmk -c
-
-# Clean-up
-rm preprint.suppinfo
-
-mv preprint* ../rendered/
-
-rm abstract.tex main.md annexes.tex appendices.tex ressources.tex template.tex lapreprint.cls 
+rm abstract.tex main.md chapters/main_docx.md \
+annexes.tex appendices.tex ressources.tex template.tex lapreprint.cls 
 
 cd ..
